5de9f3e38fd2f3077a5925ae731b0bd9fe238c40
John Stephen Degillo <johnstephen.degillo@sun-asterisk.com>
2022-04-19 16:34:55 +0800

    Task-add-hit-animation-mario

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private new SpriteRenderer spriteRenderer;
-    public new Sprite[] runSprites;
-    public new Sprite climbSprite;
-    public new Sprite jumpSprite;
-    private new Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] runSprites;
+    public Sprite climbSprite;
+    public Sprite jumpSprite;
     private int spriteIndex;
+
+    private new Rigidbody2D rigidbody;
     private new Collider2D collider;
-    private new Collider2D[] results;
+    private Collider2D[] results;
+
+    private bool isHit;
+    public Sprite[] hitSprites;
+    private int hitSpriteIdx;
+
     private Vector2 direction;
     private bool isGrounded;
     private bool isClimbing;
     public float moveSpeed = 1f;
     public float jumpStrength = 1f;
 
-    private void CheckCollision() {
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        results = new Collider2D[4];
+    }
+
+    private void CheckCollision()
+    {
         isGrounded = false;
         isClimbing = false;
 
@@ -25,7 +40,8 @@ public class Player : MonoBehaviour
         size.x /= 2f;
         int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
 
-        for (int i = 0; i < amount; i++) {
+        for (int i = 0; i < amount; i++)
+        {
             GameObject hit = results[i].gameObject;
 
             if (hit.layer == LayerMask.NameToLayer("Ground"))
@@ -33,31 +49,24 @@ public class Player : MonoBehaviour
                 isGrounded = hit.transform.position.y < (transform.position.y - 0.5f);
                 Physics2D.IgnoreCollision(collider, results[i], !isGrounded);
             }
-            else if (hit.layer == LayerMask.NameToLayer("Ladder")) {
+            else if (hit.layer == LayerMask.NameToLayer("Ladder"))
+            {
                 isClimbing = true;
             }
         }
     }
 
-    private void Awake() 
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        rigidbody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
-        results = new Collider2D[4];
-    }
-
     private void OnEnable()
     {
-        InvokeRepeating(nameof(AnimateSprite), 1f/12f, 1f/12f);
+        InvokeRepeating(nameof(AnimateSprite), 1f / 12f, 1f / 12f);
     }
-    
+
     private void OnDisable()
     {
         CancelInvoke();
     }
 
-    private void Update() 
+    private void Update()
     {
         CheckCollision();
 
@@ -69,7 +78,7 @@ public class Player : MonoBehaviour
         {
             direction = Vector2.up * jumpStrength;
         }
-        else 
+        else
         {
             direction += Physics2D.gravity * Time.deltaTime;
         }
@@ -81,7 +90,7 @@ public class Player : MonoBehaviour
             direction.y = Mathf.Max(direction.y, -1f);
         }
 
-        if (direction.x > 0f) 
+        if (direction.x > 0f)
         {
             transform.eulerAngles = Vector3.zero;
         }
@@ -94,11 +103,27 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + direction * Time.fixedDeltaTime);
+
+        if (isHit)
+        {
+            rigidbody.simulated = false;
+
+            if (hitSpriteIdx >= hitSprites.Length)
+            {
+                enabled = false;
+                FindObjectOfType<GameManager>().LevelFailed();
+            }
+        }
     }
 
-    private void AnimateSprite() 
+    private void AnimateSprite()
     {
-        if (isClimbing) 
+        if (isHit)
+        {
+            hitSpriteIdx++;
+            if (hitSpriteIdx < hitSprites.Length) spriteRenderer.sprite = hitSprites[hitSpriteIdx];
+        }
+        else if (isClimbing)
         {
             spriteRenderer.sprite = climbSprite;
         }
@@ -123,17 +148,16 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) 
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Objective")) 
+        if (collision.gameObject.CompareTag("Objective"))
         {
             enabled = false;
             FindObjectOfType<GameManager>().LevelComplete();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            enabled = false;
-            FindObjectOfType<GameManager>().LevelFailed();
+            isHit = true;
         }
     }
 }
