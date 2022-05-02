676d200bd2aa5c4b0570d0de02167981d2ff5e51
Jiggs <85467863+JoseGabrielJavier@users.noreply.github.com>
2022-05-02 15:18:45 +0800

    [TASK] Created Pause Menu
    - toggle escape key
    - resume/quit buttons

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    private bool Toggle = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Toggle == false)
+            {
+                Pause();
+            }
+            else if (Toggle == true)
+            {
+                Resume();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        PausePanel.SetActive(true);
+        Toggle = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        PausePanel.SetActive(false);
+        Toggle = false;
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
